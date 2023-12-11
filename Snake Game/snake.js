dd5c0e69ef6212bc66af97e42441e9bf38276cd0
Omprakash1353 <138029792+Omprakash1353@users.noreply.github.com>
2023-12-11 23:18:30 +0530

    Add files via upload

---
+++ Snake Game/snake.js
@@ -0,0 +1,53 @@
+import { getInputDirection } from "./input.js";
+
+export const SNAKE_SPEED = 5;
+
+const snakeBody = [{ x: 11, y: 11 }];
+
+let newSegments = 0;
+
+export function update() {
+  addSegments();
+  const inputDirection = getInputDirection();
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
+    snakeBody[i + 1] = Object.assign({}, snakeBody[i]);
+  }
+  snakeBody[0].x += inputDirection.x;
+  snakeBody[0].y += inputDirection.y;
+}
+
+export function draw(gameBoard) {
+  snakeBody.forEach((segment) => {
+    const snakeElement = document.createElement("div");
+    snakeElement.style.gridRowStart = segment.y;
+    snakeElement.style.gridColumnStart = segment.x;
+    snakeElement.classList.add("snake");
+    gameBoard.appendChild(snakeElement);
+  });
+}
+
+export function expandSnake(amount) {
+  newSegments += amount;
+}
+
+export function onSnake(position, { ignoreHead = false } = {}) {
+  return snakeBody.some((bodyPart, index) => {
+    if (ignoreHead && index === 0) return false;
+    return bodyPart.x === position.x && bodyPart.y === position.y;
+  });
+}
+
+export function getSnakeHead() {
+  return snakeBody[0];
+}
+
+export function snakeIntersection() {
+  return onSnake(snakeBody[0], { ignoreHead: true });
+}
+
+function addSegments() {
+  for (let i = 0; i < newSegments; i++) {
+    snakeBody.push({ ...snakeBody[snakeBody.length - 1] });
+  }
+  newSegments = 0;
+}
